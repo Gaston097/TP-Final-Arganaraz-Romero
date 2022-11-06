514df586c2856eea791f6992b32f26141072a268
lvingman <45737515+lvingman@users.noreply.github.com>
2022-11-06 20:09:37 -0300

    Agrega carga de datos a DB (faltan articulos)

---
+++ CargaDatos.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Metodo_Pago VALUES ('Efectivo'), ('Credito'), ('Debito'), ('Transferencia Bancaria')
+INSERT INTO Categoria VALUES ('Aire Acondicionado'), ('Televisor'), ('Celular'), ('Heladera'), ('Lavarropas'), ('Ventilador'), ('Notebook'), ('Tablet')
+INSERT INTO Marca VALUES ('Philips'), ('Drean'), ('Lenovo'), ('LG'), ('Gafa'), ('BGH'), ('Electrolux'), ('Noblex'), ('Samsung')
+INSERT INTO User_Tipo VALUES ('Invitado'), ('Usuario'), ('Empleado'), ('Admin')
+INSERT INTO Estado_Comercial VALUES ('En Stock'), ('Oferta'), ('Agotado')
